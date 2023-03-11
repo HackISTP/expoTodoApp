9e56afa4cad40e8112eb452eca2985263d895136
HackISTP <hyeminpak@gmail.com>
2023-03-11 18:30:37 +0900

    reat svg 설치 및 사용해보기

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -5,9 +5,8 @@ import CheckboxUnChecked from "../assets/checkbox-unchecked.svg";
 import DeleteIcon from "../assets/delete.svg";
 export default function TodoItem() {
   return (
-    <View>
-      <Text style={styles.itemContainer}>TodoItem</Text>
-      <Pressable style={styles.itemTextChecked} hitSlop={10}>
+    <View style={styles.itemContainer}>
+      <Pressable style={styles.itemTextCheckBox} hitSlop={10}>
         <CheckboxChecked />
         <CheckboxUnChecked style={styles.itemCheckboxCheckedIcon} />
       </Pressable>
@@ -24,8 +23,43 @@ export default function TodoItem() {
 
 const styles = StyleSheet.create({
   itemContainer: {
-    // flexDirection: "row",
-    // alignItems: "center",
-    // paddingTop: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingTop: 10,
+    paddingBottom: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#f7f8fa",
+  },
+  itemTextCheckBox: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 20,
+    height: 20,
+    marginRight: 13,
+    borderRadius: 6,
+  },
+  itemCheckboxCheckedIcon: {
+    shadowColor: "#000000",
+    shadowOpacity: 0.14,
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+  },
+  itemText: {
+    marginRight: "auto",
+    paddingRight: 25,
+    fontSize: 15,
+    lineHeight: 20,
+    color: "#737373",
+  },
+  itemTextChecked: {
+    opacity: 0.3,
+    textDecorationLine: "line-through",
+  },
+  deleteButton: { opacity: 0.8 },
+  deleteButtonDone: {
+    opacity: 0.3,
   },
 });
